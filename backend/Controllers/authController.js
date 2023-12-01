0b11ead8118aeeabe409ea3e4fb987f711e9af67
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-01 11:09:44 +0530

    added the user and doctor models and created the routes and controllers

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,17 @@
+
+export const register = async(req, res) => {
+    const {email, password, name, role, photo, gender} = req.body;
+    try {
+        let user = null;
+    } catch (error) {
+        
+    }
+}
+
+export const login = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
