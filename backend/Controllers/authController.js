6aa520e6c608d4c3cb2675237aa274e882e4c526
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-01 12:28:09 +0530

    added the api for post request on login  route

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -4,6 +4,12 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
 
+const generateToken = user => {
+    return jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET_KEY, {
+        expiresIn: "15d",
+    });
+}
+
 export const register = async(req, res) => {
     console.log(req.body);
     const {email, password, name, role, photo, gender} = req.body;
@@ -48,7 +54,6 @@ export const register = async(req, res) => {
         }
 
         await user.save();
-
         res.status(200).json({success: true, message: "User successfully created"});
 
         
@@ -59,9 +64,46 @@ export const register = async(req, res) => {
 }
 
 export const login = async(req, res) => {
+    const { email, password } = req.body;
     try {
+        let user = null;
+
+        const patient = await User.findOne({email});
+        const doctor = await Doctor.findOne({email});
+
+        if(patient){
+            user = patient;
+        }
+        if(doctor){
+            user = doctor;
+        }
+
+        // check if user exist or not
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+
+        // compare password
+        const isPassordMatch = await bcrypt.compare(req.body.password, user.password);
         
+        if(!isPassordMatch){
+            return res.status(400).json({status: false, message: "Invalid credentials"});
+        }
+
+        // get token
+        const token = generateToken(user);
+
+        const { password, role, appointments, ...rest } = user._doc;
+
+        res.status(200).json({
+            status: true,
+            message: "Successfully login",
+            token,
+            data:{...rest},
+            role
+        });
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).json({ status: false, message: "Failed to login"});
     }
 }
