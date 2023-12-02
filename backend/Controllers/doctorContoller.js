e6def10288c98527d73d1b7029ab8cd6f26936de
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-02 11:14:10 +0530

    added the search functionality in the API

--- backend/Controllers/doctorContoller.js
+++ backend/Controllers/doctorContoller.js
@@ -37,7 +37,22 @@ export const getSingleDoctor = async (req, res) => {
 }
 export const getAllDoctor = async (req, res) => {
     try {
-        const doctors = await Doctor.find({}).select("-password"); // .select("-password") will filter the data send to the Doctors, i.e it will not send the password to the Doctors..
+
+        // implementing search function
+        const { query } = req.query;
+
+        let doctors;
+        if(query){
+            doctors = await Doctor.find({
+                isApproved: "approved",
+                $or: [
+                    { name: { $regex: query, $options: "i" } },
+                    { specialization: { $regex: query, $options: "i" } },
+                ],
+            }).select("-password");
+        } else {
+            doctors = await Doctor.find({ isApproved: "approved" }).select("-password"); // .select("-password") will filter the data send to the Doctors, i.e it will not send the password to the Doctors.
+        }
 
         res.status(200).json({success: true, message: "Doctors Found", data: doctors});
     } catch (error) {
