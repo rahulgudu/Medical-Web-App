9b16dbb426154d0b6f5e895e034d7421897da60f
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-23 14:52:46 +0530

    integrated the login backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <ToastContainer theme='dark' position='top-right' autoClose={3000} closeOnClick pauseOnHover={false} />
-      <App />
+      <AuthContextProvider>
+        <ToastContainer
+          theme="dark"
+          position="top-right"
+          autoClose={3000}
+          closeOnClick
+          pauseOnHover={false}
+        />
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,65 @@
-import { useState } from "react"
-import { Link } from "react-router-dom";
+import { useState, useContext } from "react"
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../config";
+import { toast } from "react-toastify";
+import { authContext } from "../context/AuthContext";
 const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
 
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const {dispatch} = useContext(authContext);
+
   const handleInputChange = e => {
     setFormData({ ... formData, [e.target.name]: e.target.value })
+  }
+
+
+  const submitHandler = async event =>{
+    event.preventDefault();
+    console.log(formData);
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const result = await res.json();
+
+      if(!res.ok) {
+        throw new Error(result.message);
+      }
+      
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role,
+        },
+      });
+
+      console.log(result, "login data");
+
+      setLoading(false);
+      toast.success(result.message);
+      navigate("/home");
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+      setLoading(false);
+    }
+    
   }
   return (
     <div>
@@ -17,7 +69,7 @@ const Login = () => {
             Hello! <span className="text-primaryColor">Welcome</span> Back 🎉
           </h3>
 
-          <form className="py-4 md:py-0">
+          <form className="py-4 md:py-0" onSubmit={submitHandler}>
             <div className="mb-5">
               <input 
                 type="email" 
