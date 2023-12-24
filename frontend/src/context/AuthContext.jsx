d05fa5b646e1ad0b37aee74f1ccdeccb201a9eea
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-24 21:08:13 +0530

    enhanced the header after logging in and added to keep the information at local storage

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,9 +1,9 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 
 const initialState = {
-  user: "null",
-  role: "null",
-  token: "null",
+  user: localStorage.getItem("user") !== undefined ? JSON.parse(localStorage.getItem("user")) : null,
+  role: localStorage.getItem("role") || null,
+  token: localStorage.getItem("role") || null,
 };
 
 export const authContext = createContext(initialState);
@@ -38,6 +38,12 @@ const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(()=>{
+    localStorage.setItem('user', JSON.stringify(state.user));
+    localStorage.setItem("token", state.token);
+    localStorage.setItem("role", state.role);
+  }, [state]);
 
   return (
     <authContext.Provider
