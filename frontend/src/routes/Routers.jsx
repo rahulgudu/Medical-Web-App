849272acc16605d18733b7cce514211a347045f6
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-25 21:59:09 +0530

    Add ToastContainer, Signup page, and update Routers. (generated by blackbox.ai )

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -5,8 +5,12 @@ import Signup from '../pages/Signup';
 import Contact from '../pages/Contact';
 import Doctors from '../pages/Doctors/Doctors';
 import DoctorDetails from '../pages/Doctors/DoctorDetails';
+import MyAccount from "../Dashboard/user-account/MyAccount";
+import DoctorAccount from "../Dashboard/doctor-account/DoctorAccount";
 
 import { Routes, Route } from "react-router-dom";
+
+import ProtectedRoute from "./ProtectedRoute";
 const Routers = () => {
   return (
     <Routes>
@@ -18,6 +22,8 @@ const Routers = () => {
         <Route path='/register' element={<Signup />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/services' element={<Services />} />
+        <Route path='/users/profile/me' element={<ProtectedRoute allowedRoles={["patient"]}><MyAccount /></ProtectedRoute>} />
+        <Route path='/doctors/profile/me' element={<ProtectedRoute allowedRoles={["doctor"]}><DoctorAccount /></ProtectedRoute>} />
     </Routes>
   )
 }
